17159a280d33ca09cdb5763b7af1a43956b6f253
Taka Goto <tgoto@8thlight.com>
2018-08-24 16:21:02 -0500

    parking scheduling for house

--- javascripts/parking.js
+++ javascripts/parking.js
@@ -8,28 +8,7 @@ window.onload = function() {
     var tomorrowInString = (tomorrow.getMonth() + 1) + "/" + tomorrow.getDate() + "/" + tomorrow.getFullYear()
     var todayInString = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear()
 
-		house.forEach((person) => {
-
-			$("[data-id=tomorrow]").text(tomorrowInString)
-
-			var todaySchedule = person.schedules.find((s) => {
-				return s.date == todayInString
-			})
-			if (todaySchedule) {
-				$("[data-id=today-schedule]")
-					.append("<div> " + person.name + " is leaving " + todaySchedule.leaving + " and coming back " + todaySchedule.back + " </div>")
-			}
-
-			var tomorrowSchedule = person.schedules.find((s) => {
-				return s.date == tomorrowInString
-			})
-			if (tomorrowSchedule) {
-				$("[data-id=tomorrow-schedule]")
-					.append("<div> " + person.name + " is leaving " + tomorrowSchedule.leaving + " and coming back " + tomorrowSchedule.back + " </div>")
-			}
-
-			$("[data-id=name]").append("<option value=" + person.name + ">" + person.name + "</option>")
-		})
+    display(house, tomorrowInString, todayInString)
 
     $("[data-id=submit]").click(function() {
       var name = $("[data-id=name] option:selected").text()
@@ -41,8 +20,42 @@ window.onload = function() {
         data: JSON.stringify({name: name, date: tomorrowInString, leaving: leaving, back: back}),
         method: "POST"
       }).done(function(response) {
-        location.reload()
+        display(JSON.parse(response), tomorrowInString, todayInString)
       })
     })
 	})
+}
+
+function display(house, tomorrowInString, todayInString) {
+  $("[data-id=tomorrow-schedule]").empty()
+  $("[data-id=today-schedule]").empty()
+
+  house.forEach((person) => {
+
+    displayTodaySchedule(todayInString, person)
+    displayTomorrowSchedule(tomorrowInString, person)
+
+    $("[data-id=tomorrow]").text(tomorrowInString)
+    $("[data-id=name]").append("<option value=" + person.name + ">" + person.name + "</option>")
+  })
+}
+
+function displayTodaySchedule(todayInString, person) {
+  var todaySchedule = person.schedules.find((s) => {
+    return s.date == todayInString
+  })
+  if (todaySchedule) {
+    $("[data-id=today-schedule]")
+      .append("<div> " + person.name + " is leaving " + todaySchedule.leaving + " and coming back " + todaySchedule.back + " </div>")
+  }
+}
+
+function displayTomorrowSchedule(tomorrowInString, person) {
+  var tomorrowSchedule = person.schedules.find((s) => {
+    return s.date == tomorrowInString
+  })
+  if (tomorrowSchedule) {
+    $("[data-id=tomorrow-schedule]")
+      .append("<div> " + person.name + " is leaving " + tomorrowSchedule.leaving + " and coming back " + tomorrowSchedule.back + " </div>")
+  }
 }
